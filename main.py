277c6591903a11cf82633b8d4189a94e9218d60a
PeterPirog <peterpirogtf@gmail.com>
2021-07-20 23:59:43 +0200

    next commit

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from preprocessing.Transformers import IterativeImputerDf
 
 if __name__ == "__main__":
 
-    X_train=pd.DataFrame([[7, 2, 3], [4, np.nan, 6], [10, 5, 9]])
-    X_test=pd.DataFrame([[np.nan, 2, 3], [4, np.nan, 6], [10, np.nan, 9],[10, 4.5, np.nan]])
-
+    X_train=pd.DataFrame([[7, 2, 3], [4, np.nan, 6], [10, 5, 9]],columns=['aaa','bbb','ccc'])
+    X_train = pd.DataFrame([[7, 2, 3], [4, np.nan, 6], [10, 5, 9],[1,2,3]], columns=['aaa', 'bbb', 'ccc'])
+    X_test=pd.DataFrame([[np.nan, 2, 3], [4, np.nan, 6], [10, np.nan, 9], [10, 4.5, np.nan]]], columns=['aaa','bbb','ccc'])
+    print(X_train.head())
 
     imp_mean = IterativeImputerDf(random_state=0,dataframe_as_output=True)
     out_train=imp_mean.fit_transform(X_train)
