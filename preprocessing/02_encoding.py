615b1df064adee06c6edadb2fddfb5a951c37562
PeterPirog <peterpirogtf@gmail.com>
2021-07-20 21:37:04 +0200

    next commit

--- preprocessing/02_encoding.py
+++ preprocessing/02_encoding.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pathlib import Path
 from numpy import load, save
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
@@ -9,113 +10,130 @@ import xgbfir
 
 import joblib
 from category_encoders import OneHotEncoder
-from feature_engine.selection import DropConstantFeatures, DropDuplicateFeatures,SmartCorrelatedSelection
+from feature_engine.selection import DropConstantFeatures, DropDuplicateFeatures, SmartCorrelatedSelection
 from Transformers import QuantileTransformerDf, IterativeImputerDf, RareLabelNanEncoder
 
 import pandas as pd
+
 pd.set_option('display.max_columns', None)
 
 
 def make_preprocessing(config):
-    x_encoded_file = f'encoded_train_X_data.csv'
-    y_output_file=f'y_train.csv'
+    base_path=config['base_path']
 
-    x_train_enc_path = '/home/peterpirog/PycharmProjects/BostonHousesTune/data/encoded/' + x_encoded_file
-    y_train_path = '/home/peterpirog/PycharmProjects/BostonHousesTune/data/encoded/' + y_output_file
+    x_train_enc_path=(base_path / "./data_files/encoded_train_X_data.csv").resolve()
+    x_train_enc_path_xlsx = (base_path / "./data_files/encoded_train_X_data.xlsx").resolve()
+    y_train_path = (base_path / "./data_files/y_train.csv").resolve()
 
-    try:
-        df_x_enc=pd.read_csv(x_train_enc_path)
-        df_y = pd.read_csv(y_train_path)
-
-
-    except:
-        df_train = pd.read_csv('/home/peterpirog/PycharmProjects/BostonHousesTune/preprocessing/preprocessed_train_data.csv')
-        X_train=df_train.drop(['SalePrice'], axis=1).copy()
-        Y_train=df_train['SalePrice'].copy()
-        #print(X_train.head())
-        # print(Y_train.head())
+    x_test_enc_path=(base_path / "./data_files/encoded_test_X_data.csv").resolve()
+    x_test_enc_path_xlsx = (base_path / "./data_files/encoded_test_X_data.xlsx").resolve()
+    y_train_path=(base_path / "./data_files/y_test.csv").resolve()
 
 
+    #df_x_enc = pd.read_csv(x_train_enc_path)
+    #df_y = pd.read_csv(y_train_path)
 
-        # PREPROCESSING
-        # STEP 1 -  categorical features rare labels encoding
-        rle = RareLabelNanEncoder(categories=None, tol=config['rare_tol'],
-                                  minimum_occurrences=None,
-                                  n_categories=config['n_categories'],
-                                  max_n_categories=None,
-                                  replace_with='Rare',
-                                  impute_missing_label=False,
-                                  additional_categories_list=None)
 
-        # STEP 2 - categorical features one hot encoding
-        # https://github.com/scikit-learn-contrib/category_encoders/blob/master/category_encoders/one_hot.py
-        ohe = OneHotEncoder(verbose=0, cols=None, drop_invariant=False, return_df=True,
-                            handle_missing='return_nan',
-                            # options are 'error', 'return_nan', 'value', and 'indicator'.
-                            handle_unknown='return_nan',
-                            # options are 'error', 'return_nan', 'value', and 'indicator'
-                            use_cat_names=False)
+    #get data from domain encoded files
+        #TRAINING DATA
+    df_train = pd.read_csv((base_path / "./data_files/domain_train_data.csv").resolve())
+    #X_train = df_train.drop(['SalePrice','Id'], axis=1).copy()
+    #Y_train = df_train['Id','SalePrice'].copy()
 
-        # STEP 3 - numerical values quantile transformation with skewness removing
-        q_trans = QuantileTransformerDf(n_quantiles=1000, output_distribution='uniform',
-                                        ignore_implicit_zeros=False,
-                                        subsample=1e5, random_state=42, copy=True, dataframe_as_output=True,
-                                        dtype=np.float32)
+        # TEST DATA
+    df_test= pd.read_csv((base_path / "./data_files/domain_test_data.csv").resolve())
 
-        dcf=DropConstantFeatures(tol=0.95,
-                                 missing_values='ignore')
-        ddf=DropDuplicateFeatures()
+
+    X_train = df_train.drop(['SalePrice','Id'], axis=1).copy()
+    Y_train = df_train[['Id','SalePrice']].copy()
+    # print(X_train.head())
+    # print(Y_train.head())
 
 
 
-        # STEP 4 - missing values multivariate imputation
-        imp = IterativeImputerDf(min_value=0,  # values from 0 to 1 for categorical for numeric
-                                 max_value=1,
-                                 random_state=42,
-                                 initial_strategy='median',
-                                 max_iter=config['max_iter'],
-                                 tol=config['iter_tol'],
-                                 verbose=3, dataframe_as_output=True)
-        scs = SmartCorrelatedSelection(
-            variables=None,
-            method="pearson",
-            threshold=0.9,
-            missing_values="ignore",
-            selection_method="variance",
-            estimator=None,
-        )
+    # PREPROCESSING
+    # STEP 1 -  categorical features rare labels encoding
+    rle = RareLabelNanEncoder(categories=None, tol=config['rare_tol'],
+                              minimum_occurrences=None,
+                              n_categories=config['n_categories'],
+                              max_n_categories=None,
+                              replace_with='Rare',
+                              impute_missing_label=False,
+                              additional_categories_list=None)
+
+    # STEP 2 - categorical features one hot encoding
+    # https://github.com/scikit-learn-contrib/category_encoders/blob/master/category_encoders/one_hot.py
+    ohe = OneHotEncoder(verbose=0, cols=None, drop_invariant=False, return_df=True,
+                        handle_missing='return_nan',
+                        # options are 'error', 'return_nan', 'value', and 'indicator'.
+                        handle_unknown='return_nan',
+                        # options are 'error', 'return_nan', 'value', and 'indicator'
+                        use_cat_names=False)
+
+    # STEP 3 - numerical values quantile transformation with skewness removing
+    q_trans = QuantileTransformerDf(n_quantiles=1000, output_distribution='uniform',
+                                    ignore_implicit_zeros=False,
+                                    subsample=1e5, random_state=42, copy=True, dataframe_as_output=True,
+                                    dtype=np.float32)
+
+    dcf = DropConstantFeatures(tol=0.94,
+                               missing_values='ignore')
+    ddf = DropDuplicateFeatures()
+
+    # STEP 4 - missing values multivariate imputation
+    imp = IterativeImputerDf(min_value=0,  # values from 0 to 1 for categorical for numeric
+                             max_value=1,
+                             random_state=42,
+                             initial_strategy='median',
+                             max_iter=config['max_iter'],
+                             tol=config['iter_tol'],
+                             verbose=3, dataframe_as_output=True)
 
 
-        # STEP 5 MAKE PIPELINE AND TRAIN IT
-        pipeline = Pipeline([
-            ('rare_lab', rle),
-            ('one_hot', ohe),
-            ('q_trans', q_trans),
-            ('imputer', imp),
-            ('drop_quasi_const', dcf),
-            ('drop_duplicate', ddf),
-            ('smart_correlated_sel', scs),
-        ])
+    scs = SmartCorrelatedSelection(
+        variables=None,
+        method="pearson",
+        threshold=0.9,
+        missing_values="ignore",
+        selection_method="variance",
+        estimator=None,
+    )
 
-        # Pipeline training
+    # STEP 5 MAKE PIPELINE AND TRAIN IT
+    pipeline = Pipeline([
+        ('rare_lab', rle),
+        ('one_hot', ohe),
+        ('q_trans', q_trans),
+        ('drop_quasi_const1', dcf),
+        ('drop_duplicate1', ddf),
+        ('imputer', imp),
+        ('drop_quasi_const2', dcf),
+        ('drop_duplicate2', ddf),
+        ('smart_correlated_sel', scs),
+    ])
 
-        X_train_encoded=pipeline.fit_transform(X_train)
-        print('X_train_encoded',type(X_train_encoded))
-        # save X_train_encoded array
-        #save(file=train_enc_path, arr=X_train_encoded)
-        #save(file=y_train_path, arr=Y_train)
+    # Pipeline training
 
-        # save trained pipeline
-        #joblib.dump(pipeline, pipeline_path)
-        df_train_encoded = pd.concat([X_train_encoded,Y_train], axis=1)
-        print(df_train_encoded.head())
-        df_train_encoded.to_csv(path_or_buf='/home/peterpirog/PycharmProjects/BostonHousesTune/preprocessing/encoded_train_data.csv',
-                      sep=',',
-                      header=True,
-                      index=False)
-        df_train_encoded.to_excel('/home/peterpirog/PycharmProjects/BostonHousesTune/preprocessing/encoded_train_data.xlsx',
-                        sheet_name='output_data',
-                        index=False)
+    X_train_encoded = pipeline.fit_transform(X_train)
+    print('X_train_encoded', type(X_train_encoded))
+    # save X_train_encoded array
+    # save(file=train_enc_path, arr=X_train_encoded)
+    # save(file=y_train_path, arr=Y_train)
+
+    # save trained pipeline
+    # joblib.dump(pipeline, pipeline_path)
+    df_train_encoded = pd.concat([X_train_encoded, Y_train], axis=1)
+    print(df_train_encoded.head())
+
+    df_train_encoded.to_csv(
+        path_or_buf=x_train_enc_path,
+        sep=',',
+        header=True,
+        index=False)
+    df_train_encoded.to_excel(
+        x_train_enc_path_xlsx,
+        sheet_name='output_data',
+        index=False)
 
     # STEP 7 SPLITTING DATA FOR KERAS
     X_train, X_test, y_train, y_test = train_test_split(X_train_encoded, Y_train,
@@ -123,8 +141,8 @@ def make_preprocessing(config):
                                                         test_size=0.2,
                                                         random_state=42)
 
-    return X_train_encoded,Y_train
-    #return X_train, X_test, y_train, y_test
+    return X_train_encoded, Y_train
+    # return X_train, X_test, y_train, y_test
 
 
 def rmsle(y_true, y_pred, **kwargs):
@@ -138,6 +156,9 @@ def rmsle(y_true, y_pred, **kwargs):
 
 
 if __name__ == "__main__":
+    # define path to the project directory
+    base_path = Path(__file__).parent.parent
+
     # https://machinelearningmastery.com/xgboost-for-regression/
     config = {
         # PREPROCESSING
@@ -147,7 +168,8 @@ if __name__ == "__main__":
         # Iterative imputer
         "max_iter": 30,
         "iter_tol": 0.001,
-        "output": 'df'
+        "output": 'df',
+        'base_path': base_path
     }
 
     import joblib
@@ -155,9 +177,7 @@ if __name__ == "__main__":
 
     register_ray()
     with joblib.parallel_backend('ray'):
-
-        X,y = make_xgb_preprocessing(config=config)
+        X, y = make_preprocessing(config=config)
 
         print(X.head())
         print(f'The input shape is:{X.shape}')
-
