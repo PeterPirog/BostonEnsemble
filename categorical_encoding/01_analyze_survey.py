594283068c1ea37a464bf2c64cf6e579abfb2155
peterpirogtf <peterpirogtf@gmail.com>
2021-08-17 12:59:50 +0200

    initial commit

---
+++ categorical_encoding/01_analyze_survey.py
@@ -0,0 +1,94 @@
+#https://github.com/david26694/QE_experiments/blob/master/DecisionTreePlot_ohe_te_qe.ipynb
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+
+
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor, plot_tree
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error,mean_squared_error
+from matplotlib import pyplot as plt
+
+from catboost import CatBoostRegressor
+from category_encoders.m_estimate import MEstimateEncoder
+from category_encoders.one_hot import OneHotEncoder
+import sktools
+
+
+
+if __name__ == "__main__":
+    df = pd.read_csv(
+        "/home/peterpirog/PycharmProjects/BostonEnsemble/categorical_encoding/developer_survey_2020/survey_results_public.csv")
+    features = ['MainBranch', 'Hobbyist', 'Age', 'Age1stCode', 'CompFreq', 'Country', 'CurrencyDesc',
+                'CurrencySymbol', 'DatabaseDesireNextYear', 'DatabaseWorkedWith',
+                'DevType', 'EdLevel', 'Employment', 'Ethnicity', 'Gender', 'JobFactors',
+                'JobSat', 'JobSeek', 'LanguageDesireNextYear', 'LanguageWorkedWith',
+                'MiscTechDesireNextYear', 'MiscTechWorkedWith',
+                'NEWCollabToolsDesireNextYear', 'NEWCollabToolsWorkedWith', 'NEWDevOps',
+                'NEWDevOpsImpt', 'NEWEdImpt', 'NEWJobHunt', 'NEWJobHuntResearch',
+                'NEWLearn', 'NEWOffTopic', 'NEWOnboardGood', 'NEWOtherComms',
+                'NEWOvertime', 'NEWPurchaseResearch', 'NEWPurpleLink', 'NEWSOSites',
+                'NEWStuck', 'OpSys', 'OrgSize', 'PlatformDesireNextYear',
+                'PlatformWorkedWith', 'PurchaseWhat', 'Sexuality', 'SOAccount',
+                'SOComm', 'SOPartFreq', 'SOVisitFreq', 'SurveyEase', 'SurveyLength',
+                'Trans', 'UndergradMajor', 'WebframeDesireNextYear',
+                'WebframeWorkedWith', 'WelcomeChange', 'WorkWeekHrs', 'YearsCode',
+                'YearsCodePro']
+
+    target = 'ConvertedComp'  # 'CompTotal', 'ConvertedComp'
+    X = df[features]
+    y = df[target]
+
+    X_tr, X_te, y_tr, y_te = train_test_split(X, y, random_state=0)
+
+    te = MEstimateEncoder()
+    pe = sktools.encoders.QuantileEncoder(quantile=0.5,m=1)
+    ohe = OneHotEncoder()
+
+    X_tr_ohe = ohe.fit_transform(X_tr)
+    X_tr_te = te.fit_transform(X_tr, y_tr)
+    X_tr_pe = pe.fit_transform(X_tr, y_tr)
+
+    dt = DecisionTreeRegressor(random_state=0, max_depth=6, ccp_alpha=0.1)
+
+    #ONE HOT
+    dt.fit(X_tr_ohe, y_tr)
+
+    print(mean_absolute_error(dt.predict(X_tr_ohe), y_tr))
+    print(mean_absolute_error(dt.predict(ohe.transform(X_te)), y_te))
+
+    plt.figure()
+    plot_tree(dt, filled=True)
+    plt.savefig('tree_ohe.eps', format='eps')
+    plt.show()
+
+    #Target Encodings
+    dt.fit(X_tr_te, y_tr)
+
+    print(mean_absolute_error(dt.predict(X_tr_te), y_tr))
+    print(mean_absolute_error(dt.predict(te.transform(X_te)), y_te))
+
+    plt.figure()
+    plot_tree(dt, filled=True)
+    plt.savefig('tree_te.eps', format='eps')
+    plt.show()
+
+    #QUANTILE
+    dt.fit(X_tr_pe, y_tr)
+
+    print(mean_absolute_error(dt.predict(X_tr_pe), y_tr))
+    print(mean_absolute_error(dt.predict(pe.transform(X_te)), y_te))
+
+    plt.figure()
+    plot_tree(dt, filled=True)
+    plt.show()
+
+
+
+
+
+
+
+
